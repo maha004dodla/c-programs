04018005b222e2fe7312ee7d92eb2529a429925f
maha004dodla <97425755+maha004dodla@users.noreply.github.com>
2022-01-22 11:17:18 +0530

    Add files via upload

---
+++ largest element in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n],i,max;
+	for(i=0; i<n; i++)
+	{
+		scanf("%d ",&arr[i]);
+	}
+	max = arr[0];
+	for(i=0; i<n; i++)
+	{
+		if(arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("%d ", max);
+}
---
+++ reading  the digits from 0-9.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("zero");
+	}
+	else if(n==1)
+	{
+		printf("one\n");
+	}
+	else if(n==2)
+	{
+		printf("two\n");
+	}
+	else if(n==3)
+	{
+		printf("three\n");
+	}
+	else if(n==4)
+	{
+		printf("four\n");
+	}
+	else if(n==5)
+	{
+		printf("five\n");
+	}
+	else if(n==6)
+	{
+		printf("six\n");
+	}
+	else if(n==7)
+	{
+		printf("seven\n");
+	}
+	else if(n==8)
+    {
+        printf("eight\n");
+    }
+    else 
+    {
+    	printf("nine");
+	}
+}
